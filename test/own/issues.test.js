694ea710d40c84095f95c839509b3a16919fa7d7
Pikku Myy <pikkumyy@Amir-Air-2.lan>
2021-11-23 18:14:11 +0200

    Add 10.4

---
+++ test/own/issues.test.js
@@ -0,0 +1,59 @@
+decribe('Products need to be connected to Database instead of reading JSON file', () => {
+    it('Products should be found from db', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Product Schema needs to be designed', () => {
+    it('Schema should exist', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Add own Mocha Tests', () => {
+    it('Tests should be implemented', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Add Coverage to Mocha tests', () => {
+    it('Coverage shoud be over 80%', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Implement GitOps', () => {
+    it('GitOps should be implemented', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Implement features for Non-logged-in Users', () => {
+    it('Features should be implemented', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Create User Schema for Order', () => {
+    it('Orders User Schema should be implemented', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Deploy Application to Heroku', () => {
+    it('Application should be deployed to Heroku', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Implement Navigation for payment page', () => {
+    it('Payment page navigation should be implemented', () => {
+        throw 'Not yet implemented';
+    });
+});
+
+decribe('Fixing ESLint issues', () => {
+    it('No EsLint errors should exist', () => {
+        throw 'Not yet implemented';
+    });
+});
