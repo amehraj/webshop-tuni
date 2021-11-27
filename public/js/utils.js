749f77b6886536ecb5b95d21cb304a6fd065c45b
Anna Knappe <anna.knappe@tuni.fi>
2021-11-27 23:35:40 +0200

    JSDoc errors fixed

--- public/js/utils.js
+++ public/js/utils.js
@@ -34,7 +34,7 @@ const getJSON = async url => {
  *
  * @param {string} url resource url on the server
  * @param {string} method "PUT" or "POST"
- * @param {Object|Array} data payload data to be sent to the server as JSON
+ * @param {object|Array} data payload data to be sent to the server as JSON
  * @returns {Promise<*>} promise that resolves to the parsed JSON
  */
 const postOrPutJSON = async(url, method, data = {}) => {
@@ -93,7 +93,7 @@ const deleteResource = async url => {
  * or other HTML elements (remember that IDs must be unique within
  * a document).
  *
- * @returns {string}
+ * @returns {string} Returns the generated id as string
  */
 const generateId = () => {
   // Shamelessly borrowed from a Gist. See:
@@ -112,7 +112,7 @@ const generateId = () => {
  * Appends a new paragraph inside the container element and gives it
  * class based on the status of the message (success or failure).
  *
- * @param {string} message
+ * @param {string} message The notification message to be displayed
  * @param {string} containerId id attribute of the container element
  * @param {boolean} isSuccess whether the message describes a success or a failure
  */
